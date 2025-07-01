a987ffea5954e1b0e0a006bc81a68d1d481e4d4e
Mynameajit <132321923+Mynameajit@users.noreply.github.com>
2025-07-01 23:54:22 +0530

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -79,13 +79,16 @@ export const LoginUser = catchAsyncError(async (req, res, next) => {
 });
 
 export const logout = catchAsyncError(async (req, res, next) => {
-  res.cookie('token', null, {
-    expires: new Date(Date.now()),
-    httpOnly: true
+  res.clearCookie('token', {
+    httpOnly: true,
+    sameSite: 'Lax',  
+    secure: true      
   });
+
   res.status(200).json({
     success: true,
     message: "Logged Out Successfully"
   });
 });
+
 
